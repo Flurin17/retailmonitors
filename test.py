f30c8aa0a17b74aa75ee85a88c7ac46099a05bd5
sorry <flurin.laim@gmail.com>
2021-09-07 08:08:46 +0200

    testing

--- test.py
+++ test.py
@@ -40,58 +40,83 @@ def sendWebhook(Graphicscard):
         color=0x5CDBF0,
         timestamp='now'  # sets the timestamp to current time
         )
-    embed.set_author(name=Graphicscard.name, url=Graphicscard.link)
-    embed.add_field(name='Price', value=str(Graphicscard.price))
-    embed.add_field(name='delivery', value=Graphicscard.delivery)
-    embed.set_thumbnail(Graphicscard.picture)
-    print(embed.fields)
+    embed.set_author(name=Graphicscard["name"], url=Graphicscard["link"])
+    embed.add_field(name='Price', value=str(Graphicscard["price"]))
+    embed.add_field(name='delivery', value=Graphicscard["delivery"])
+    embed.set_thumbnail(Graphicscard["picture"])
+
     hook.send(embed=embed)
 
-    log("Sent webhook")
+    log("Sent webhook for {}".format(Graphicscard["name"]))
 
+class monitor:
+    def __init__(self):
+        pass
 
-def find_all_products(allproducts):
-    allProducts = allproducts[0]["data"]["productType"]["filterProductsV4"]["products"]["resultsWithDefaultOffer"]
+    def find_all_products(self):
+        self.allCards = []
+        allProducts = self.allproducts[0]["data"]["productType"]["filterProductsV4"]["products"]["resultsWithDefaultOffer"]
 
-    for product in allProducts:
-        productId = product["product"]["productId"]
-        name = product["product"]["name"]
-        url = "https://www.digitec.ch/product/{}".format(product["offer"]["productId"])
-        picture = product["product"]["images"][0]["url"]
-        delivery = product["offer"]["deliveryOptions"]["mail"]["classification"]
-        try:
-            price = float(product["offer"]["price"]["amountIncl"])
-        except:
-            price = 0.0
-
-        card = Graphicscard(productId, name, url, price, delivery, picture)
-        sendWebhook(card)
-        print(asdict(card))
+        for product in allProducts:
+            productId = product["product"]["productId"]
+            name = product["product"]["name"]
+            url = "https://www.digitec.ch/product/{}".format(product["offer"]["productId"])
+            picture = product["product"]["images"][0]["url"]
+            delivery = product["offer"]["deliveryOptions"]["mail"]["classification"]
+            try:
+                price = float(product["offer"]["price"]["amountIncl"])
+            except:
+                price = 0.0
+            card = Graphicscard(productId, name, url, price, delivery, picture)
+            #sendWebhook(card)
+            self.allCards.append(asdict(card))
+            log(asdict(card)["name"])
   
 
-def getProducts():
-    while True:
-        try:
-            r = requests.post(
-            url='https://www.digitec.ch/api/graphql',
-            headers={
-                'user-agent': 'PostmanRuntime/7.28.4',
-                'Accept': '*/*',
-                'Accept-Encoding': 'gzip, deflate, br',
-                'Connection': 'keep-alive',
-                str(random.choice(range(11111111111,99999999999))):str(random.choice(range(11111111111,99999999999)))
-            },
-            json=[{"operationName":"GET_PRODUCT_TYPE_PRODUCTS_AND_FILTERS","variables":{"productTypeId":106,"queryString":"oo=1&pdo=39-347%3A677785%7C39-347%3A677897%7C39-347%3A651564%7C39-347%3A651565%7C39-347%3A722095%7C39-347%3A722097%7C39-347%3A721845%7C39-347%3A721850%7C39-347%3A629038","offset":0,"limit":84,"sort":"BESTSELLER","siteId":"0","sectorId":1},"query":"query GET_PRODUCT_TYPE_PRODUCTS_AND_FILTERS($productTypeId: Int!, $queryString: String!, $offset: Int, $limit: Int, $sort: ProductSort, $siteId: String, $sectorId: Int) {  productType(id: $productTypeId) {    filterProductsV4(queryString: $queryString, offset: $offset, limit: $limit, sort: $sort, siteId: $siteId, sectorId: $sectorId) {      productCounts {        total        filteredTotal        __typename      }      productFilters {        filterGroupType        label        key        tooltip {          ...Tooltip          __typename        }        ...CheckboxFilterGroup        ...RangeSliderFilterGroup        __typename      }      products {        hasMore        resultsWithDefaultOffer {          ...ProductWithOffer          __typename        }        __typename      }      __typename    }    __typename  }}fragment Tooltip on Tooltip {  text  moreInformationLink  __typename}fragment CheckboxFilterGroup on CheckboxFilterGroupV2 {  filterOptions {    ...Filter    __typename  }  __typename}fragment RangeSliderFilterGroup on RangeSliderFilterGroupV2 {  dataPoints {    ...RangeSliderDataPoint    __typename  }  selectedRange {    min    max    __typename  }  optionIdentifierKey  unitAbbreviation  unitDisplayOrder  totalCount  fullRange {    min    max    __typename  }  stepSize  mergeInfo {    isBottomMerged    isTopMerged    __typename  }  __typename}fragment ProductWithOffer on ProductWithOffer {  mandatorSpecificData {    ...ProductMandatorSpecific    __typename  }  product {    ...ProductMandatorIndependent    __typename  }  offer {    ...ProductOffer    __typename  }  __typename}fragment Filter on Filter {  optionIdentifierKey  optionIdentifierValue  label  productCount  selected  tooltip {    ...Tooltip    __typename  }  __typename}fragment RangeSliderDataPoint on RangeSliderDataPoint {  value  productCount  __typename}fragment ProductMandatorSpecific on MandatorSpecificData {  isBestseller  isDeleted  showroomSites  sectorIds  __typename}fragment ProductMandatorIndependent on ProductV2 {  id  productId  name  nameProperties  productTypeId  productTypeName  brandId  brandName  averageRating  totalRatings  totalQuestions  isProductSet  images {    url    height    width    __typename  }  energyEfficiency {    energyEfficiencyColorType    energyEfficiencyLabelText    energyEfficiencyLabelSigns    energyEfficiencyImage {      url      height      width      __typename    }    __typename  }  seo {    seoProductTypeName    seoNameProperties    productGroups {      productGroup1      productGroup2      productGroup3      productGroup4      __typename    }    gtin    __typename  }  lowQualityImagePlaceholder  hasVariants  smallDimensions  basePrice {    priceFactor    value    __typename  }  __typename}fragment ProductOffer on OfferV2 {  id  productId  offerId  shopOfferId  price {    amountIncl    amountExcl    currency    fraction    __typename  }  deliveryOptions {    mail {      classification      futureReleaseDate      __typename    }    pickup {      siteId      classification      futureReleaseDate      __typename    }    detailsProvider {      productId      offerId      quantity      type      __typename    }    __typename  }  label  type  volumeDiscountPrices {    minAmount    price {      amountIncl      amountExcl      currency      __typename    }    isDefault    __typename  }  salesInformation {    numberOfItems    numberOfItemsSold    isEndingSoon    validFrom    __typename  }  incentiveText  isIncentiveCashback  isNew  isSalesPromotion  hideInProductDiscovery  canAddToBasket  hidePrice  insteadOfPrice {    type    price {      amountIncl      amountExcl      currency      fraction      __typename    }    __typename  }  minOrderQuantity  __typename}"}]
-            )
-        except Exception as e:
-            log(e)
-            continue
-        
-        if r.status_code == 200:
-            find_all_products(r.json())
-            break
-        else:
-            print(r.status_code)
-            print(r.text)
+    def getProducts(self):
+        while True:
+            try:
+                r = requests.post(
+                url='https://www.digitec.ch/api/graphql',
+                headers={
+                    'user-agent': 'PostmanRuntime/7.28.4',
+                    'Accept': '*/*',
+                    'Accept-Encoding': 'gzip, deflate, br',
+                    'Connection': 'keep-alive',
+                    str(random.choice(range(11111111111,99999999999))):str(random.choice(range(11111111111,99999999999)))
+                },
+                json=[{"operationName":"GET_PRODUCT_TYPE_PRODUCTS_AND_FILTERS","variables":{"productTypeId":106,"queryString":"oo=1&pdo=39-347%3A677785%7C39-347%3A677897%7C39-347%3A651564%7C39-347%3A651565%7C39-347%3A722095%7C39-347%3A722097%7C39-347%3A721845%7C39-347%3A721850%7C39-347%3A629038","offset":0,"limit":84,"sort":"BESTSELLER","siteId":"0","sectorId":1},"query":"query GET_PRODUCT_TYPE_PRODUCTS_AND_FILTERS($productTypeId: Int!, $queryString: String!, $offset: Int, $limit: Int, $sort: ProductSort, $siteId: String, $sectorId: Int) {  productType(id: $productTypeId) {    filterProductsV4(queryString: $queryString, offset: $offset, limit: $limit, sort: $sort, siteId: $siteId, sectorId: $sectorId) {      productCounts {        total        filteredTotal        __typename      }      productFilters {        filterGroupType        label        key        tooltip {          ...Tooltip          __typename        }        ...CheckboxFilterGroup        ...RangeSliderFilterGroup        __typename      }      products {        hasMore        resultsWithDefaultOffer {          ...ProductWithOffer          __typename        }        __typename      }      __typename    }    __typename  }}fragment Tooltip on Tooltip {  text  moreInformationLink  __typename}fragment CheckboxFilterGroup on CheckboxFilterGroupV2 {  filterOptions {    ...Filter    __typename  }  __typename}fragment RangeSliderFilterGroup on RangeSliderFilterGroupV2 {  dataPoints {    ...RangeSliderDataPoint    __typename  }  selectedRange {    min    max    __typename  }  optionIdentifierKey  unitAbbreviation  unitDisplayOrder  totalCount  fullRange {    min    max    __typename  }  stepSize  mergeInfo {    isBottomMerged    isTopMerged    __typename  }  __typename}fragment ProductWithOffer on ProductWithOffer {  mandatorSpecificData {    ...ProductMandatorSpecific    __typename  }  product {    ...ProductMandatorIndependent    __typename  }  offer {    ...ProductOffer    __typename  }  __typename}fragment Filter on Filter {  optionIdentifierKey  optionIdentifierValue  label  productCount  selected  tooltip {    ...Tooltip    __typename  }  __typename}fragment RangeSliderDataPoint on RangeSliderDataPoint {  value  productCount  __typename}fragment ProductMandatorSpecific on MandatorSpecificData {  isBestseller  isDeleted  showroomSites  sectorIds  __typename}fragment ProductMandatorIndependent on ProductV2 {  id  productId  name  nameProperties  productTypeId  productTypeName  brandId  brandName  averageRating  totalRatings  totalQuestions  isProductSet  images {    url    height    width    __typename  }  energyEfficiency {    energyEfficiencyColorType    energyEfficiencyLabelText    energyEfficiencyLabelSigns    energyEfficiencyImage {      url      height      width      __typename    }    __typename  }  seo {    seoProductTypeName    seoNameProperties    productGroups {      productGroup1      productGroup2      productGroup3      productGroup4      __typename    }    gtin    __typename  }  lowQualityImagePlaceholder  hasVariants  smallDimensions  basePrice {    priceFactor    value    __typename  }  __typename}fragment ProductOffer on OfferV2 {  id  productId  offerId  shopOfferId  price {    amountIncl    amountExcl    currency    fraction    __typename  }  deliveryOptions {    mail {      classification      futureReleaseDate      __typename    }    pickup {      siteId      classification      futureReleaseDate      __typename    }    detailsProvider {      productId      offerId      quantity      type      __typename    }    __typename  }  label  type  volumeDiscountPrices {    minAmount    price {      amountIncl      amountExcl      currency      __typename    }    isDefault    __typename  }  salesInformation {    numberOfItems    numberOfItemsSold    isEndingSoon    validFrom    __typename  }  incentiveText  isIncentiveCashback  isNew  isSalesPromotion  hideInProductDiscovery  canAddToBasket  hidePrice  insteadOfPrice {    type    price {      amountIncl      amountExcl      currency      fraction      __typename    }    __typename  }  minOrderQuantity  __typename}"}]
+                )
+            except Exception as e:
+                log(e)
+                continue
+            
+            if r.status_code == 200:
+                self.allproducts = r.json()
+                self.find_all_products()
+                break
+            else:
+                log(r.status_code)
+                log(r.text)
+    
+    def compareCards(self):
+        if self.firstTime:
+            self.allCardsOld = self.allCards
 
-getProducts()
+        differentCards = [i for i in self.allCardsOld if i not in self.allCards]
+        log(differentCards)
+        for differentCard in differentCards:
+            sendWebhook(differentCard)
+
+        self.allCardsOld = self.allCards
+
+    def start(self):
+        self.firstTime = True
+        while True:
+            self.getProducts()
+            self.compareCards()
+            time.sleep(1)
+            self.firstTime = False
+
+
+monitor().start()
